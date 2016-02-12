1aff66ff75a6c219e53c31656d0e8337708fc21b
Fabricio Cabral <fabriciofx@gmail.com>
2016-02-12 02:05:50 -0300

    Refatoração para a validação do e-mail

---
+++ src/main/java/com/github/fabriciofx/rocket/validacao/ValidacaoEmail.java
@@ -0,0 +1,19 @@
+package com.github.fabriciofx.rocket.validacao;
+
+public final class ValidacaoEmail<T extends CharSequence>
+		implements Restricao<T> {
+	private final String EMAIL_REGEX =
+			"^[\\w\\-]+(\\.[\\w\\-]+)*@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$";
+
+	public ValidacaoEmail() {
+	}
+
+	@Override
+	public void valida(final T objeto) {
+		try {
+			new ValidacaoRegEx<>(EMAIL_REGEX).valida(objeto);
+		} catch (final IllegalArgumentException e) {
+			throw new IllegalArgumentException("endereço de e-mail inválido");
+		}
+	}
+}
