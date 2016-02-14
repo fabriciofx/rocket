959d7dda22abb26bfb0ecf8be634974b6465284f
Fabricio Cabral <fabriciofx@gmail.com>
2016-02-14 02:30:42 -0300

    Corrigida quebra do gerador de RGs

--- src/test/java/com/github/fabriciofx/rocket/dominio/gerador/GeradorRG.java
+++ src/test/java/com/github/fabriciofx/rocket/dominio/gerador/GeradorRG.java
@@ -3,27 +3,24 @@ package com.github.fabriciofx.rocket.dominio.gerador;
 import java.time.LocalDate;
 
 import com.github.fabriciofx.rocket.dominio.Rg;
+import com.github.fabriciofx.rocket.dominio.endereco.Estado;
 import com.github.fabriciofx.rocket.misc.Aleatorio;
 
 public final class GeradorRG {
-	private static final String[] ESTADOS = { "AC", "AL", "AM", "AP", "BA",
-			"CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE",
-			"PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO" };
-
 	private final Aleatorio aleatorio;
-	
+
 	public GeradorRG() {
 		aleatorio = new Aleatorio();
 	}
 
 	public Rg get() {
-		final String numero = String.valueOf(
-				aleatorio.numero(100000000L, 999999999L));
+		final String numero = String
+				.valueOf(aleatorio.numero(100000000L, 999999999L));
 		final Rg.Emissor[] emissores = Rg.Emissor.values();
 		final Rg.Emissor emissor = emissores[aleatorio.numero(0,
 				emissores.length - 1)];
-		final String estado = ESTADOS[aleatorio.numero(0,
-				ESTADOS.length - 1)];
+		final Estado[] estados = Estado.values();
+		final Estado estado = estados[aleatorio.numero(0, estados.length - 1)];
 		final int via = aleatorio.numero(1, 2);
 
 		return new Rg(numero, emissor, estado, via, LocalDate.now());
