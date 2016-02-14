a92c51683fbdec0172387e5cf7d085369d0a975a
Fabricio Cabral <fabriciofx@gmail.com>
2016-02-13 23:41:39 -0300

    Ajuste no algoritmo Modulo 11

--- src/main/java/com/github/fabriciofx/rocket/restricao/RestModulo11.java
+++ src/main/java/com/github/fabriciofx/rocket/restricao/RestModulo11.java
@@ -8,21 +8,19 @@ public final class RestModulo11<T extends CharSequence> extends Restricao<T> {
 	private static <T> T valida(final T objeto) {
 		final String digitos = objeto.toString();
 		final int tamanho = digitos.length();
-		final int tamanho1 = tamanho - 1;
-		final int tamanho2 = tamanho - 2;
-		final int dv1 = digitos.charAt(tamanho2) - '0';
-		final int dv2 = digitos.charAt(tamanho1) - '0';
+		final int dv1 = digitos.charAt(tamanho - 2) - '0';
+		final int dv2 = digitos.charAt(tamanho - 1) - '0';
 		int v1 = 0, v2 = 0;
 
-		for (int i = 0; i < tamanho2; i++) {
-			final int digito = digitos.charAt(tamanho2 - 1 - i) - '0';
-			v1 = v1 + digito * (tamanho2 - (i % tamanho1));
-			v2 = v2 + digito * (tamanho2 - ((i + 1) % tamanho1));
+		for (int i = 0; i < tamanho - 2; i++) {
+			final int digito = digitos.charAt(tamanho - 3 - i) - '0';
+			v1 = v1 + digito * (9 - (i % 10));
+			v2 = v2 + digito * (9 - ((i + 1) % 10));
 		}
 		
-		v1 = (v1 % tamanho) % tamanho1;
-		v2 = v2 + v1 * tamanho2;
-		v2 = (v2 % tamanho) % tamanho1;
+		v1 = (v1 % 11) % 10;
+		v2 = v2 + v1 * 9;
+		v2 = (v2 % 11) % 10;
 
 		if (v1 != dv1 && v2 != dv2) {
 			throw new IllegalArgumentException(
