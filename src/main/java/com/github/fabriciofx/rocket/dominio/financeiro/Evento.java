188c7f1f35f38975ee1e5c0f9e952da16d85af22
Fabricio Cabral <fabriciofx@gmail.com>
2016-05-02 18:58:36 -0300

    Melhoria da semântica do evento

--- src/main/java/com/github/fabriciofx/rocket/dominio/financeiro/Evento.java
+++ src/main/java/com/github/fabriciofx/rocket/dominio/financeiro/Evento.java
@@ -2,7 +2,7 @@ package com.github.fabriciofx.rocket.dominio.financeiro;
 
 import java.time.LocalDateTime;
 
-public interface Evento<Originario, Alvo> {
+public interface Evento<Origem, Destino> {
 	// Quando o evento ocorreu
 	public LocalDateTime ocorrido();
 
@@ -10,8 +10,8 @@ public interface Evento<Originario, Alvo> {
 	public LocalDateTime observado();
 
 	// Quem originou o evento
-	public Originario originario();
+	public Origem origem();
 
 	// Quem recebeu o evento
-	public Alvo alvo();
+	public Destino destino();
 }
