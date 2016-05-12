4190277013c1838a5474f7d9a4a15ad4cd981d20
Fabricio Cabral <fabriciofx@gmail.com>
2016-05-12 02:34:38 -0300

    Corrigido o bug de não criptografar a senha logo em sua criação

--- src/main/java/com/github/fabriciofx/rocket/dominio/Senha.java
+++ src/main/java/com/github/fabriciofx/rocket/dominio/Senha.java
@@ -5,25 +5,24 @@ import java.io.IOException;
 import com.github.fabriciofx.rocket.infra.crypto.Hash;
 import com.github.fabriciofx.rocket.infra.crypto.Sha256;
 import com.github.fabriciofx.rocket.misc.Hex;
+import com.github.fabriciofx.rocket.restricao.RestNaoNulo;
 
-public final class Senha extends Literal {
+public final class Senha {
 	private final transient Hash hash;
+	private final transient String conteudo;
 
-	public Senha(final String conteudo) {
+	public Senha(final String conteudo) throws IOException {
 		this(new Sha256(), conteudo);
 	}
 
-	public Senha(final Hash hash, final String conteudo) {
-		super(conteudo);
-		this.hash = hash;
+	public Senha(final Hash hash, final String conteudo) throws IOException {
+		this.hash = new RestNaoNulo<Hash>().valido(hash);
+		this.conteudo = new Hex(hash).conteudo(
+				new RestNaoNulo<String>().valido(conteudo).getBytes());
 	}
 
 	@Override
 	public String toString() {
-		try {
-			return new Hex(hash).conteudo(super.toString().getBytes());
-		} catch (final IOException e) {
-			throw new IllegalArgumentException(e);
-		}
+		return conteudo;
 	}
 }
