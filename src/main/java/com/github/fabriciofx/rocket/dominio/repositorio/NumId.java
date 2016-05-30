f02b9941275aec56b3a796683f31323397f8e5b3
Fabricio Cabral <fabriciofx@gmail.com>
2016-05-29 23:17:30 -0300

    Adicionado NumId, identificador de entidades no formato de número

---
+++ src/main/java/com/github/fabriciofx/rocket/dominio/repositorio/NumId.java
@@ -0,0 +1,30 @@
+package com.github.fabriciofx.rocket.dominio.repositorio;
+
+public final class NumId implements Id, Comparable<NumId> {
+	private final transient long numero;
+
+	public NumId(final long numero) {
+		this.numero = numero;
+	}
+
+	@Override
+	public int compareTo(final NumId numId) {
+		return (int) (numero - numId.numero);
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) (31 * numero);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		return o != null && o instanceof NumId
+				&& numero == NumId.class.cast(o).numero;
+	}
+
+	@Override
+	public long toLong() {
+		return numero;
+	}
+}
