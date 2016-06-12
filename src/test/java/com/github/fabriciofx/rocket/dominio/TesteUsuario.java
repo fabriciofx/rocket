8af3c8f57418e439b44f21b030c2f541acc9e933
Fabricio Cabral <fabriciofx@gmail.com>
2016-06-12 01:21:35 -0300

    Corrigido bug da tradução

--- src/test/java/com/github/fabriciofx/rocket/dominio/TesteUsuario.java
+++ src/test/java/com/github/fabriciofx/rocket/dominio/TesteUsuario.java
@@ -7,12 +7,13 @@ import java.io.IOException;
 import org.junit.Test;
 
 import com.github.fabriciofx.rocket.security.Password;
+import com.github.fabriciofx.rocket.system.DefaultUser;
 import com.github.fabriciofx.rocket.system.User;
 
 public final class TesteUsuario {
 	@Test
 	public void padrao() throws IOException {
-		final User u = new User.Default(new Nome("Homer Simpson"),
+		final User u = new DefaultUser(new Nome("Homer Simpson"),
 				new Email("homer@fox.com"), new Password("D'oh!"));
 		assertEquals("Homer Simpson", u.name().completo());
 		assertEquals("homer@fox.com", u.email().toString());
