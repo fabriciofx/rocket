89e7ace24da8c7f81a40f02331e1d9734ad7e113
Fabricio Cabral <fabriciofx@gmail.com>
2016-07-18 14:04:49 -0300

    Adicionada exceção ao print

--- src/main/java/com/github/fabriciofx/rocket/media/Printer.java
+++ src/main/java/com/github/fabriciofx/rocket/media/Printer.java
@@ -1,5 +1,7 @@
 package com.github.fabriciofx.rocket.media;
 
+import java.io.IOException;
+
 public interface Printer {
-	Media print(Media media);
+	Media print(Media media) throws IOException;
 }
--- src/main/java/com/github/fabriciofx/rocket/range/Range.java
+++ src/main/java/com/github/fabriciofx/rocket/range/Range.java
@@ -1,9 +1,7 @@
 package com.github.fabriciofx.rocket.range;
 
-import com.github.fabriciofx.rocket.media.Media;
+import com.github.fabriciofx.rocket.media.Printer;
 
-public interface Range<T extends Comparable<T>> {
+public interface Range<T extends Comparable<T>> extends Printer {
 	boolean contains(T value);
-	
-	Media print(Media media);
 }
