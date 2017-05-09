405022345d89ae125e1803539976005f39813f36
Fabricio Cabral <fabriciofx@gmail.com>
2017-05-09 19:57:45 -0300

    StringFormat like more a String

--- src/main/java/com/github/fabriciofx/rocket/string/StringFormat.java
+++ src/main/java/com/github/fabriciofx/rocket/string/StringFormat.java
@@ -1,6 +1,10 @@
 package com.github.fabriciofx.rocket.string;
 
-public final class StringFormat {
+import java.io.Serializable;
+
+public final class StringFormat
+	implements Serializable, Comparable<StringFormat>, CharSequence {
+	private static final long serialVersionUID = 5196167298744019069L;
 	private final String format;
 	private final Object[] args;
 
@@ -11,6 +15,26 @@ public final class StringFormat {
 
 	@Override
 	public String toString() {
-		return String.format(format, args);
+		return String.format(this.format, this.args);
+	}
+
+	@Override
+	public int length() {
+		return this.toString().length();
+	}
+
+	@Override
+	public char charAt(final int index) {
+		return this.toString().charAt(index);
+	}
+
+	@Override
+	public CharSequence subSequence(final int start, final int end) {
+		return this.toString().subSequence(start, end);
+	}
+
+	@Override
+	public int compareTo(final StringFormat o) {
+		return this.toString().compareTo(o.toString());
 	}
 }
